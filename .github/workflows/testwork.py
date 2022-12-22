57f18f681f7531814dd3c09729607184085f282f
byunjuneseok <byunjuneseok@gmail.com>
2022-12-22 12:32:50 +0900

    asd

---
+++ .github/workflows/testwork.py
@@ -0,0 +1,13 @@
+import os
+
+if __name__ == '__main__':
+    print('*' * 80)
+    print(os.environ)
+    print('*' * 80)
+    print(os.environ['GITHUB_REPOSITORY_OWNER'])
+    print('*' * 80)
+    print(os.environ['GITHUB_REPOSITORY'].split('/')[1])
+    print('*' * 80)
+    print(os.environ['GITHUB_REPOSITORY_NAME'])
+    print('*' * 80)
+    print(os.environ['GITHUB_TOKEN'])
